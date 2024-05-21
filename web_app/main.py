cdd2d59fe468bce2db1d2b2b4a763312159d85e5
Roland Nagy <81804897+rolandnagy-ds@users.noreply.github.com>
2024-05-21 14:07:07 +0200

    Update main.py removed api key

--- web_app/main.py
+++ web_app/main.py
@@ -28,7 +28,7 @@ def fetch_data(start_date, end_date):
     # Replace 'CHANNEL_ID' with your ThingSpeak channel ID
 
     # Your ThingSpeak read API key
-    api_key = 'IXT4T5APCGA3QSMF'
+    api_key = 'your-api-key'
 
     # Query parameters for the API request
     params = {'api_key': api_key, 'start': start_date.strftime('%Y-%m-%dT%H:%M:%SZ'), 'end': end_date.strftime('%Y-%m-%dT%H:%M:%SZ')} 
@@ -98,7 +98,7 @@ app.layout = html.Div([
         figure={
             'data': [],
             'layout': go.Layout(
-                title='Utolsó 5 mért érték',
+                title='Last 5 value',
                 xaxis={'title': 'Date'},
                 yaxis={'title': 'Mass'}
             )
@@ -107,7 +107,7 @@ app.layout = html.Div([
     html.Div([
     dcc.Markdown('''
         #### Dash and Markdown
-        **Descripton to here**
+        **Descripton to here (in the future)**
     ''')
     ]),
     dcc.Interval(
@@ -183,7 +183,7 @@ def update_graph(timeframe, start_date, end_date, n_intervals):
 )
 
 def func(n_clicks):
-    dates, values, dates_weather, temp = fetch_data((datetime.now() - timedelta(days=100)).strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d'))  # Example
+    dates, values, dates_weather, temp = fetch_data((datetime.now() - timedelta(days=100)).strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d'))
     df = pd.DataFrame({'Dates':dates,'Mass': values})  # Adjust data as needed
     return dcc.send_data_frame(df.to_csv, "mydf.csv", index = False)
 
@@ -192,152 +192,3 @@ def func(n_clicks):
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-"""
-# Function to fetch data from ThingSpeak
-def fetch_data(start_date, end_date):
-    if start_date == 'last_3_values':
-        start_date = datetime.now() - timedelta(days=5)
-    else:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-    if end_date == 'today':
-        end_date = datetime.now()
-    else:
-        end_date = datetime.strptime(end_date, '%Y-%m-%d')
-
-    # ThingSpeak read API endpoint for the specified timeframe
-    url = 'https://api.thingspeak.com/channels/{}/feeds.json'.format('2448118')
-    # Replace 'CHANNEL_ID' with your ThingSpeak channel ID
-
-    # Your ThingSpeak read API key
-    api_key = 'IXT4T5APCGA3QSMF'
-
-    # Query parameters for the API request
-    params = {'api_key': api_key, 'start': start_date.strftime('%Y-%m-%dT%H:%M:%SZ'), 'end': end_date.strftime('%Y-%m-%dT%H:%M:%SZ')} 
-
-    # Fetch data from ThingSpeak
-    response = requests.get(url, params=params)
-
-    # Extract data from response
-    data = response.json()
-
-    # Extract values and dates from the specified timeframe
-    values = [float(entry['field1']) for entry in data['feeds']]
-    # Adjust for time zone difference
-    dates = [datetime.strptime(entry['created_at'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Europe/Budapest')) - timedelta(hours=0) for entry in data['feeds']]
-    #dates = [datetime.strptime(entry['created_at'], '%Y-%m-%dT%H:%M:%SZ') for entry in data['feeds']]
-
-    return dates, values
-
-# Create Dash app
-app = dash.Dash(__name__)
-server = app.server
-
-# Define layout of the app
-app.layout = html.Div([
-    html.H1("N. Z. - Hive Scale Data", style={'textAlign': 'center'}),
-    html.Label('Select timeframe:'),
-    dcc.Dropdown(
-        id='timeframe-dropdown',
-        options=[
-            {'label': 'Last 3 days', 'value': 'last_3_values'},
-            {'label': 'Custom timeframe', 'value': 'custom_timeframe'}
-        ],
-        value='last_3_values',
-        clearable=False
-    ),
-    html.Div(id='date-picker-container', style={'display': 'none'}, children=[
-        html.Label('Start date:'),
-        dcc.DatePickerSingle(
-            id='start-date-picker',
-            display_format='YYYY-MM-DD',
-            style={'marginBottom': 10}
-        ),
-        html.Label('End date:'),
-        dcc.DatePickerSingle(
-            id='end-date-picker',
-            display_format='YYYY-MM-DD',
-            style={'marginBottom': 10}
-        )
-    ]),
-    dcc.Graph(
-        id='line-chart',
-        figure={
-            'data': [],
-            'layout': go.Layout(
-                title='Utolsó 5 mért érték',
-                xaxis={'title': 'Date'},
-                yaxis={'title': 'Mass'}
-            )
-        }
-    ),
-    dcc.Interval(
-        id='interval-component',
-        interval=5*60*1000,  # in milliseconds # Currently 5 minutes
-        n_intervals=0
-    )
-])
-
-# Callback to display date pickers based on selected timeframe
-@app.callback(
-    Output('date-picker-container', 'style'),
-    [Input('timeframe-dropdown', 'value')]
-)
-def display_date_pickers(timeframe):
-    if timeframe == 'custom_timeframe':
-        return {'display': 'block'}
-    else:
-        return {'display': 'none'}
-
-# Callback to update the graph data
-@app.callback(
-    Output('line-chart', 'figure'),
-    [Input('timeframe-dropdown', 'value'),
-     Input('start-date-picker', 'date'),
-     Input('end-date-picker', 'date'),
-     Input('interval-component', 'n_intervals')]
-)
-def update_graph(timeframe, start_date, end_date, n_intervals):
-    if timeframe == 'custom_timeframe':
-        if start_date is None or end_date is None:
-            return {'data': [], 'layout': go.Layout(title='Custom timeframe selected')}
-        else:
-            dates, values = fetch_data(start_date, end_date)
-            return {
-                'data': [
-                    go.Scatter(
-                        x=dates,
-                        y=values,
-                        mode='lines+markers',
-                        name='Field Values'
-                    )
-                ],
-                'layout': go.Layout(
-                    title='Custom timeframe selected',
-                    xaxis={'title': 'Date'},
-                    yaxis={'title': 'Mass'}
-                )
-            }
-    else:
-        dates, values = fetch_data('last_3_values', 'today')
-        return {
-            'data': [
-                go.Scatter(
-                    x=dates,
-                    y=values,
-                    mode='lines+markers',
-                    name='Field Values'
-                )
-            ],
-            'layout': go.Layout(
-                title='Last 3 days selected',
-                xaxis={'title': 'Date'},
-                yaxis={'title': 'Mass'}
-            )
-        }
-
-# Run the Dash app
-if __name__ == '__main__':
-    app.run_server(debug=True)
-"""
